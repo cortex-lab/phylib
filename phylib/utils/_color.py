8f65c709572ea64015c33c8fc4c234943d8660a7
Cyrille Rossant <rossant@users.noreply.github.com>
2019-05-10 15:31:35 +0200

    More complete categorical colormap

--- phylib/utils/_color.py
+++ phylib/utils/_color.py
@@ -6,6 +6,7 @@
 # Imports
 #------------------------------------------------------------------------------
 
+import colorcet as cc
 import numpy as np
 from numpy.random import uniform
 from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
@@ -26,10 +27,18 @@ def _random_color(h_range=(0., 1.),
 
 
 def _is_bright(rgb):
-    """Return whether a RGB color is bright or not."""
-    r, g, b = rgb
-    gray = 0.299 * r + 0.587 * g + 0.114 * b
-    return gray >= .5
+    """Return whether a RGB color is bright or not.
+    see https://stackoverflow.com/a/3943023/1595060
+    """
+    L = 0
+    for c, coeff in zip(rgb, (0.2126, 0.7152, 0.0722)):
+        if c <= 0.03928:
+            c = c / 12.92
+        else:
+            c = ((c + 0.055) / 1.055) ** 2.4
+        L += c * coeff
+    if (L + 0.05) / (0.0 + 0.05) > (1.0 + 0.05) / (L + 0.05):
+        return True
 
 
 def _random_bright_color():
@@ -40,25 +49,21 @@ def _random_bright_color():
     return rgb
 
 
+def _hex_to_triplet(h):
+    # Convert an hexadecimal color to a triplet of int8 integers.
+    if h.startswith('#'):
+        h = h[1:]
+    return tuple(int(h[i:i + 2], 16) for i in (0, 2, 4))
+
+
 #------------------------------------------------------------------------------
 # Colormap
 #------------------------------------------------------------------------------
 
 # Default color map for the selected clusters.
-_COLORMAP = np.array([[8, 146, 252],
-                      [255, 2, 2],
-                      [240, 253, 2],
-                      [228, 31, 228],
-                      [2, 217, 2],
-                      [255, 147, 2],
-                      [212, 150, 70],
-                      [205, 131, 201],
-                      [201, 172, 36],
-                      [150, 179, 62],
-                      [95, 188, 122],
-                      [129, 173, 190],
-                      [231, 107, 119],
-                      ])
+# see https://colorcet.pyviz.org/user_guide/Categorical.html
+_COLORMAP = np.array([_hex_to_triplet(h) for h in cc.glasbey_light])
+_COLORMAP[[0, 1, 2, 3, 4]] = _COLORMAP[[3, 0, 4, 1, 2]]
 
 
 def _apply_color_masks(color, masks=None, alpha=None):
