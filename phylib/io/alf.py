9a3d8818d0212b3418fd9ff260a8ffe9f4887b3b
Olivier Winter <olivier.winter@hotmail.fr>
2019-08-02 23:04:24 +0100

    remove ks2 subfolder

--- phylib/io/alf.py
+++ phylib/io/alf.py
@@ -45,10 +45,10 @@ _FILE_RENAMES = [  # file_in, file_out, squeeze (bool to squeeze vector from mat
     ('templates.npy', 'clusters.templateWaveforms.npy', False),
     ('cluster_Amplitude.tsv', 'clusters.amps.tsv', False),
     ('channel_map.npy', 'channels.rawRow.npy', True),
-    ('spike_templates.npy', 'ks2/spikes.clusters.npy', True),
-    ('cluster_ContamPct.tsv', 'ks2/clusters.ContamPct.tsv', False),
-    ('cluster_group.tsv', 'ks2/clusters.phyAnnotation.tsv', False),
-    ('cluster_KSLabel.tsv', 'ks2/clusters.group.tsv', False),
+    # ('spike_templates.npy', 'ks2/spikes.clusters.npy', True),
+    # ('cluster_ContamPct.tsv', 'ks2/clusters.ContamPct.tsv', False),
+    # ('cluster_group.tsv', 'ks2/clusters.phyAnnotation.tsv', False),
+    # ('cluster_KSLabel.tsv', 'ks2/clusters.group.tsv', False),
 ]
 
 FILE_DELETES = [
