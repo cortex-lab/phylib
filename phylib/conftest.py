c9430f02e77962264f395bd99bb242ebbfdd83db
Cyrille Rossant <rossant@users.noreply.github.com>
2019-12-26 18:12:06 +0100

    Remove matplotlib

--- phylib/conftest.py
+++ phylib/conftest.py
@@ -13,7 +13,6 @@ import tempfile
 import shutil
 import warnings
 
-import matplotlib
 import numpy as np
 from pytest import fixture
 
@@ -31,7 +30,6 @@ add_default_handler(5, logger=logger)
 # Fix the random seed in the tests.
 np.random.seed(2015)
 
-warnings.filterwarnings('ignore', category=matplotlib.cbook.mplDeprecation)
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
