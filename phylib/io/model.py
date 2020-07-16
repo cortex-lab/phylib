WIP: fix units in template model

phylib/io/model.py
@@ -1341,8 +1341,7 @@ class TemplateModel(object):
         logger.debug("Save spike clusters to `%s`.", path)
         np.save(path, spike_clusters)
 
-    def save_spikes_subset_waveforms(self, max_n_spikes_per_template=None, max_n_channels=None,
-                                     sample2unit=1.):
+    def save_spikes_subset_waveforms(self, max_n_spikes_per_template=None, max_n_channels=None):
         if self.traces is None:
             logger.warning(
                 "Spike waveforms could not be extracted as the raw data file is not available.")
@@ -1392,7 +1391,7 @@ class TemplateModel(object):
         # Extract waveforms from the raw data on a chunk by chunk basis.
         export_waveforms(
             path, self.traces, self.spike_samples[spike_ids], spike_channels,
-            n_samples_waveforms=self.n_samples_waveforms, sample2unit=sample2unit)
+            n_samples_waveforms=self.n_samples_waveforms)
 
         # Reload spike waveforms.
         self.spike_waveforms = self._load_spike_waveforms()

phylib/io/tests/conftest.py
@@ -136,11 +136,11 @@ def _make_mock_dataset(tempdir):
 
     # Params.
     n_channels = 164
-    n_channels_loc = 32
-    n_spikes = 5_000
-    n_clusters = 112
-    n_samples_waveforms = 82
-    sample_rate = 15e3
+    n_channels_loc = 24
+    n_spikes = 4_000
+    n_clusters = 72
+    n_samples_waveforms = 62
+    sample_rate = 5e3
 
     n_templates = n_clusters
 
@@ -179,14 +179,18 @@ def _make_mock_dataset(tempdir):
         perm = np.random.permutation(ch)
         templates[i, :, perm] *= amp[:, np.newaxis]
         template_channels[i, :] = ch
-    _save('templates.npy', templates.astype(np.float32))
-    _save('templates_ind.npy', template_channels)
 
     # Raw data.
     # for simplicity, assume all spikes are complete on the raw data
     duration = spike_samples[-1] + n_samples_waveforms
-    traces = artificial_traces(duration, n_channels)
-    (50000 * traces / traces.max()).astype(np.int16).tofile(root / 'raw.dat')
+    traces = 1e-3 * artificial_traces(duration, n_channels)
+    factor = 50000 / traces.max()
+    (factor * traces).astype(np.int16).tofile(root / 'raw.dat')
+
+    # NOTE: need to take the factor into account in the templates
+    templates *= 1e-3 * factor
+    _save('templates.npy', templates.astype(np.float32))
+    _save('templates_ind.npy', template_channels)
 
     write_text(root / 'params.py', dedent(f'''
         dat_path = 'raw.dat'
@@ -195,8 +199,8 @@ def _make_mock_dataset(tempdir):
         offset = 0
         sample_rate = {sample_rate}
         hp_filtered = False
-        ampfactor = 1.0 / 1e4
-    '''))
+        ampfactor = 1.0 / %.5e
+    ''' % factor))
     return root / 'params.py'
 
 

phylib/io/traces.py
@@ -650,21 +650,20 @@ def iter_waveforms(traces, spike_samples, spike_channels, n_samples_waveforms=No
 
 
 def export_waveforms(
-        path, traces, spike_samples, spike_channels, n_samples_waveforms=None, cache=False,
-        sample2unit=1):
+        path, traces, spike_samples, spike_channels, n_samples_waveforms=None, cache=False):
     """Export a selection of spike waveforms to a npy file by iterating over the data on a chunk
     by chunk basis."""
     n_spikes = len(spike_samples)
     spike_channels = np.asarray(spike_channels, dtype=np.int32)
     n_channels_loc = spike_channels.shape[1]
     shape = (n_spikes, n_samples_waveforms, n_channels_loc)
-    dtype = traces.dtype if sample2unit is None else np.float
+    dtype = traces.dtype
     writer = NpyWriter(path, shape, dtype)
     size_written = 0
     for waveforms in iter_waveforms(
             traces, spike_samples, spike_channels, n_samples_waveforms=n_samples_waveforms,
             cache=cache):
-        writer.append(waveforms * sample2unit)
+        writer.append(waveforms)
         size_written += waveforms.size
     writer.close()
     assert prod(shape) == size_written