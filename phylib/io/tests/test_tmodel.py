57c845bc2ebdbeff1d877d639f8b1d4f33ec8532
Cyrille Rossant <rossant@users.noreply.github.com>
2020-10-23 14:36:01 +0200

    More tests and fixes in new Model

--- phylib/io/tests/test_tmodel.py
+++ phylib/io/tests/test_tmodel.py
@@ -13,7 +13,7 @@ import shutil
 import tempfile
 import unittest
 
-# import numpy as np
+import numpy as np
 # import numpy.random as npr
 # from numpy.testing import assert_allclose as ac
 # from pytest import raises
@@ -49,3 +49,54 @@ class TemplateModelDenseTests(unittest.TestCase):
 
     def test_1(self):
         assert len(self.model.template_ids) >= 50
+        assert len(self.model.template_ids) == self.model.n_templates
+
+        for tid in self.model.template_ids:
+            template = self.model.template(tid)
+            assert template.n_spikes > 0
+            assert template.amplitude > 0
+            assert len(template.channel_ids) > 0
+
+            # Template waveforms.
+            w = template.waveforms
+            amps = w.max(axis=0) - w.min(axis=0)
+            assert np.all(np.diff(amps) <= 0)
+
+    def test_2(self):
+        assert len(self.model.cluster_ids) >= 50
+        assert len(self.model.cluster_ids) == self.model.n_clusters
+
+        for cid in self.model.cluster_ids:
+            cluster = self.model.cluster(cid)
+            assert cluster.n_spikes > 0
+            assert cluster.amplitude > 0
+            assert len(cluster.channel_ids) > 0
+
+            # Cluster waveforms.
+            w = cluster.waveforms
+            amps = w.max(axis=0) - w.min(axis=0)
+            assert np.all(np.diff(amps) <= 0)
+
+            if cid in self.model.template_ids:
+                assert cluster.template_ids == [cid]
+                assert cluster.template_counts == [len(self.model.spc[cid])]
+
+    def test_3(self):
+        for cid in self.model.cluster_ids:
+            n = self.model.cluster(cid).n_spikes
+            assert self.model.spike_amps(cid).shape == (n,)
+            assert self.model.spike_depths(cid).shape == (n,)
+
+            # TODO: proper tests
+            self.model.mean_spike_waveforms(cid)
+            self.model.spike_waveforms(cid)
+            self.model.pc_features(cid)
+            self.model.template_features(cid)
+
+    def test_4(self):
+        assert self.model.channel_map.shape == (self.model.n_channels,)
+        assert self.model.channel_positions.shape == (self.model.n_channels, 2)
+        if self.model.channel_shanks is not None:
+            assert self.model.channel_shanks.shape == (self.model.n_channels,)
+        if self.model.channel_probes is not None:
+            assert self.model.channel_probes.shape == (self.model.n_channels,)
--- phylib/io/tmodel.py
+++ phylib/io/tmodel.py
@@ -18,8 +18,8 @@ import logging
 
 import numpy as np
 
-from .array import _spikes_per_cluster
-from .loader import from_sparse
+from .array import _spikes_per_cluster, _index_of
+from .loader import from_sparse as from_sparse_
 from phylib.utils import Bunch
 
 logger = logging.getLogger(__name__)
@@ -29,16 +29,23 @@ logger = logging.getLogger(__name__)
 # Utils
 #------------------------------------------------------------------------------
 
-def from_sparse_bidim(data=None, rows=None, cols=None, channel_ids=None, spike_ids=None):
-    assert spike_ids is not None
-    spike_ids = np.intersect1d(spike_ids, rows)
-    if not len(spike_ids):
-        return
+def from_sparse(data, cols, channel_ids):
+    # HACK: transpose so that the channels are in the last dimension
     if data.ndim == 3:
         data = np.transpose(data, (0, 2, 1))
-    dense = from_sparse(data, cols, channel_ids)
+    dense = from_sparse_(data, cols, channel_ids)
     if data.ndim == 3:
         dense = np.transpose(dense, (0, 2, 1))
+    return dense
+
+
+def from_sparse_bidim(data=None, rows=None, cols=None, channel_ids=None, spike_ids=None):
+    assert spike_ids is not None
+    if rows is not None:
+        spike_ids = np.intersect1d(spike_ids, rows)
+    if not len(spike_ids):
+        return
+    dense = from_sparse(data, cols, channel_ids)
     return Bunch(data=dense, rows=spike_ids, cols=channel_ids)
 
 
@@ -58,6 +65,9 @@ class TModel:
         self.loader = loader
         self.spc = _spikes_per_cluster(self.loader.spike_clusters)
 
+        self.n_spikes = len(self.loader.spike_clusters)
+        self.n_channels = len(self.loader.channel_map)
+
         self.template_ids = np.unique(self.loader.spike_templates)
         self.n_templates = len(self.template_ids)
 
@@ -68,6 +78,9 @@ class TModel:
     # ----------------------
 
     def template(self, tid):
+        # TODO: reorder channels by decreasing template amplitude? or not needed?
+        # w = self.loader.templates.data[tid]
+        # amps = w.max(axis=0) - w.min(axis=0)
         return Bunch(
             waveforms=self.loader.templates.data[tid],
             channel_ids=self.loader.templates.cols[tid],
@@ -103,10 +116,9 @@ class TModel:
         # Waveforms
         if return_waveforms:
             out.waveforms = from_sparse(
-                np.transpose(self.loader.templates.data[template_ids], (0, 2, 1)),
-                np.transpose(self.loader.templates.cols[template_ids], (0, 2, 1)),
+                self.loader.templates.data[template_ids],
+                self.loader.templates.cols[template_ids],
                 channel_ids)
-            out.waveforms = np.transpose(out.waveforms, (0, 2, 1))
             out.waveforms = np.average(out.waveforms, weights=counts, axis=0)
             assert out.waveforms.ndim == 2
         return out
@@ -131,6 +143,8 @@ class TModel:
         return self._idx(self.loader.spike_depths, cid)
 
     def spike_waveforms(self, cid, spike_ids=None):
+        if not getattr(self.loader, 'spike_waveforms', None):
+            return
         if spike_ids is None:
             spike_ids = self.spc[cid]
         sw = self.loader.spike_waveforms
@@ -141,23 +155,24 @@ class TModel:
 
     def mean_spike_waveforms(self, cid):
         w = self.spike_waveforms(cid)
-        return w.mean(axis=0)
+        if w is not None:
+            return w.mean(axis=0)
+
+    def _features(self, obj, cid):
+        spike_ids = self.spc[cid]
+        channel_ids = self.cluster(cid, return_waveforms=False).channel_ids
+        # 3D sparse array
+        return from_sparse_bidim(
+            data=obj.data,
+            rows=obj.rows,
+            cols=obj.cols[_index_of(self.loader.spike_templates, self.template_ids)],
+            channel_ids=channel_ids, spike_ids=spike_ids)  # the columns and rows we want to get
 
     def pc_features(self, cid):
-        spike_ids = self.spc[cid]
-        fet = self.loader.features
-        channel_ids = self.cluster(cid, return_waveforms=False)
-        return from_sparse_bidim(
-            data=fet.data, rows=fet.rows, cols=fet.cols,  # 3D sparse array
-            channel_ids=channel_ids, spike_ids=spike_ids)  # the columns and rows we want to get
+        return self._features(self.loader.features, cid)
 
     def template_features(self, cid):
-        spike_ids = self.spc[cid]
-        fet = self.loader.template_features
-        channel_ids = self.cluster(cid, return_waveforms=False)
-        return from_sparse_bidim(
-            data=fet.data, rows=fet.rows, cols=fet.cols,  # 2D sparse array
-            channel_ids=channel_ids, spike_ids=spike_ids)  # the columns and rows we want to get
+        return self._features(self.loader.template_features, cid)
 
     # Properties
     # ----------
